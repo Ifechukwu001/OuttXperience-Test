478ec3303d4f28eaa0c6f37a715eb6f3acf5826e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-04 18:51:27 +0100

    refactor: Update book routes to use BookRetrieve model for response

--- src/app/routers/books.py
+++ src/app/routers/books.py
@@ -8,13 +8,13 @@ from ..models import transactions
 router = routing.APIRouter()
 
 
-@router.get("/books/", response_model=list[books.Book])
+@router.get("/books/", response_model=list[books.BookRetrieve])
 def get_books(db: Session = Depends(get_db)):
     books = transactions.get_books(db)
     return books
 
 
-@router.get("/books/{id}", response_model=books.Book)
+@router.get("/books/{id}", response_model=books.BookRetrieve)
 def get_book(id: int, db: Session = Depends(get_db)):
     book = transactions.get_book(db, id)
     if book is None:
@@ -22,13 +22,13 @@ def get_book(id: int, db: Session = Depends(get_db)):
     return book
 
 
-@router.post("/books/", response_model=books.Book, status_code=201)
+@router.post("/books/", response_model=books.BookRetrieve, status_code=201)
 def create_book(book_data: books.Book, db: Session = Depends(get_db)):
     book = transactions.create_book(db, book_data)
     return book
 
 
-@router.put("/books/{id}", response_model=books.Book)
+@router.put("/books/{id}", response_model=books.BookRetrieve)
 def update_book(id: int, book_data: books.Book, db: Session = Depends(get_db)):
     book = transactions.update_book(db, id, book_data)
     if book is None:
