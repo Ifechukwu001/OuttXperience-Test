cab1b76e6a4fcd761624fe24913b84a73a57ac9a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-04 18:47:52 +0100

    feat: Add get_test_db function for managing test database connections

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -1,8 +1,16 @@
-from .models.database import SessionLocal
+from .models.database import SessionLocal, TestSessionLocal
 
 
 def get_db():
     db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_test_db():
+    db = TestSessionLocal()
     try:
         yield db
     finally:
