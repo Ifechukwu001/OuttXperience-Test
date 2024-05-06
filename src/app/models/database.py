25c84d11d077502e37ae36b761cfb9a29a35cfae
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-06 14:50:47 +0100

    refactor: Update database.py to use StaticPool for test database connection

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -1,16 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.sqlite3"
 
-TESTSQLALCHEMY_DATABASE_URL = "sqlite:///./test_app.sqlite3"
+TESTSQLALCHEMY_DATABASE_URL = "sqlite://"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 testengine = create_engine(
-    TESTSQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    TESTSQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
